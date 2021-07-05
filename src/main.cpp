0d53683cef041b1c5df31a3da0dad00c6a562f7a
MatiAKAwannabeProgrammer <mati01227@gmail.com>
2021-07-05 14:07:37 +0200

    exit with q

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,9 @@ int main()
             if (event.type == sf::Event::Closed) {
                 window.close();
             }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Q) {
+                    window.close();
+            }
         }
         window.clear();
         
