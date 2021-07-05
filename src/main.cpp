f0ab07b8c57a92933a7f20525b51671f1d242f9f
Piotr Wójtowicz <piotrekwjtowicz@yahoo.pl>
2021-07-05 14:19:52 +0200

    dodanie kom

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 
 int main()
 {
+    //komentarz
     sf::RenderWindow window(sf::VideoMode(1920, 1080), "first project", sf::Style::Fullscreen);
 
     sf::RectangleShape square(sf::Vector2f(50.f, 50.f));
