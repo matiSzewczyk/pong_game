891cd6aa1fefe1185e28e43372cd461dfec0e623
MatiAKAwannabeProgrammer <mati01227@gmail.com>
2021-07-05 13:47:13 +0200

    first square on the screen

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(1920, 1080), "first project");
+
+    sf::RectangleShape square(sf::Vector2f(50.f, 50.f));
+    square.setFillColor(sf::Color::Red);
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+        window.clear();
+        
+        window.draw(square);
+        
+        window.display();
+    }
+    return EXIT_SUCCESS;
+}
